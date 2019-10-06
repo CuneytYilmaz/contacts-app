f097185badfa7de6cfa6a1cc290e84157e6e7099
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-10-07 01:02:05 +0300

    Remove contact from database when deleted in the app

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ class App extends Component {
       contacts: currenctState.contacts.filter(c => 
         c.id !== contact.id
       )
-    }))
+    }));
+
+    ContactsAPI.remove(contact);
   }
 
   render() {
